dd7fca4dedda93ea0b0ca1ccddeb8e6741fe3e77
JIE <JIE>
2016-05-02 20:39:05 +0800

    'firstweb'

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,3 @@
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/logo.jpg') {
-      myImage.setAttribute ('src','images/logo2.jpg');
-    } else {
-      myImage.setAttribute ('src','images/logo.jpg');
-    }
-}
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 function setUserName() {
